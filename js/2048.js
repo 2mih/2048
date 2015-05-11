a93f8e666a3318721e03ba7fa38960af8e0915a8
2mih <liuch9011@hotmail.com>
2015-05-11 22:32:44 +0800

    balabala

--- js/2048.js
+++ js/2048.js
@@ -2,23 +2,25 @@
  * Created by Leo on 2015/5/1.
  */
 
-function Grid(n, t) {
+function Grid(n, id) {
 
     var _tiles = [];
 
     for (var i = n - 1; i >= 0; i--) {
         var row = [];
-        for (var j = n - 1; j >= 0; j--) {
-            row[j] = 0;
-        }
+        row[n - 1] = undefined;
         _tiles[i] = row;
     }
 
     this.tiles = _tiles;
-    this.table  = t;
+    this.trash = [];
+    this.seq = 1;
+    this.separatorSize = 10;
+    this.tileSize = 100;
+    this.id = id;
 }
 
-Grid.prototype.born = function() {
+Grid.prototype.born = function () {
 
     var i,
         j,
@@ -26,51 +28,79 @@ Grid.prototype.born = function() {
         locations = [],
         size = this.tiles.length;
 
-    for (i = size - 1; i >= 0; i--){
+    for (i = size - 1; i >= 0; i--) {
         row = this.tiles[i];
-        for (j = size - 1; j>= 0; j--){
+        for (j = size - 1; j >= 0; j--) {
 
-            if(this.tiles[i][j] === 0){
+            if (!this.tiles[i][j]) {
                 locations[locations.length] = i + ',' + j;
             }
         }
     }
 
     // a new tile born with 2 or 4 randomly
-    var newLocation = locations[Math.floor(Math.random() * locations.length)].split(',');
-    this.tiles[newLocation[0]][newLocation[1]] = Math.floor(Math.random() * 2 + 1);
+    var newLocation = locations[Math.floor(Math.random() * locations.length)].split(','),
+        n = Math.floor(Math.random() * 2 + 1);
+    this.tiles[newLocation[0]][newLocation[1]] = new Tile(n, newLocation[0], newLocation[1]);
 
 };
 
-Grid.prototype.merge = function(line) {
+Grid.prototype.merge = function () {
 
     var process = [],
-        tmp = true,
+        size = this.tiles.length,
+        avai = true,
+        tmp,
         cur,
-        length;
-    for (var i = 0; i < line.length;i++) {
-        cur = line[i];
-        length = process.length;
-        if(cur !== 0) {
-            if(tmp && line[i] === process[length - 1]) {
-                process[length - 1]++;
-                tmp = false;
-            } else {
-                process[length] = cur;
-                tmp = true;
+        line;
+
+    for (var j = 0; j < size; j++) {
+        line = this.tiles[j];
+        for (var i = 0; i < size; i++) {
+            cur = line[i];
+            if (cur) {
+                if (tmp) {
+                    if (tmp.pow === cur.pow) {
+                        this.trash.push(tmp);
+                        tmp = undefined;
+
+                        cur.pow++;
+                        process.push(cur);
+                    } else {
+                        process.push(tmp);
+                        tmp = cur;
+                    }
+                } else {
+                    tmp = cur;
+                }
             }
         }
+
+        if (tmp) {
+            process.push(tmp);
+        }
+
+        // 补足长度
+        if (!process[size - 1]) {
+            process[size - 1] = undefined;
+        }
+
+        // 步骤是否有效
+        if (avai) {
+            for (var k = 0; k < size; k++) {
+                avai = avai && (line[k] === process[k]);
+            }
+        }
+
+        this.tiles[j] = process;
+        tmp = undefined;
+        process = [];
     }
 
-    // 补0
-    for (var j = process.length; j < line.length; j++){
-        process[j] = 0;
-    }
-
-    return process;
+    return avai;
 };
 
-Grid.prototype.rotateRight90 = function(){
+Grid.prototype.rotateRight90 = function () {
     var process = [],
         size = this.tiles.length - 1;
 
@@ -86,7 +116,7 @@ Grid.prototype.rotateRight90 = function(){
     this.tiles = process;
 };
 
-Grid.prototype.rotateLeft90 = function(){
+Grid.prototype.rotateLeft90 = function () {
     var process = [],
         size = this.tiles.length - 1;
 
@@ -102,7 +132,7 @@ Grid.prototype.rotateLeft90 = function(){
     this.tiles = process;
 };
 
-Grid.prototype.rotate180 = function(){
+Grid.prototype.rotate180 = function () {
     var process = [],
         size = this.tiles.length - 1;
 
@@ -111,44 +141,40 @@ Grid.prototype.rotate180 = function(){
         for (var j = 0; j < row.length; j++) {
             var cell = row[j];
             process[size - i] = process[size - i] || [];
-            process[size - i][size -j] = cell;
+            process[size - i][size - j] = cell;
         }
     }
 
     this.tiles = process;
 };
 
-Grid.prototype.slideUp = function(){
+Grid.prototype.slideUp = function () {
     this.rotateLeft90();
-    for (var i = 0; i < this.tiles.length; i++) {
-        this.tiles[i] = this.merge(this.tiles[i]);
-    }
+    var avai = this.merge();
     this.rotateRight90();
+    return avai;
 };
 
-Grid.prototype.slideLeft = function(){
-    for (var i = 0; i < this.tiles.length; i++) {
-        this.tiles[i] = this.merge(this.tiles[i]);
-    }
+Grid.prototype.slideLeft = function () {
+    var avai = this.merge();
+    return avai;
 };
 
-Grid.prototype.slideRight = function(){
+Grid.prototype.slideRight = function () {
     this.rotate180();
-    for (var i = 0; i < this.tiles.length; i++) {
-        this.tiles[i] = this.merge(this.tiles[i]);
-    }
+    var avai = this.merge();
     this.rotate180();
+    return avai;
 };
 
-Grid.prototype.slideDown = function(){
+Grid.prototype.slideDown = function () {
     this.rotateRight90();
-    for (var i = 0; i < this.tiles.length; i++) {
-        this.tiles[i] = this.merge(this.tiles[i]);
-    }
+    var avai = this.merge();
     this.rotateLeft90();
+    return avai;
 };
 
-Grid.prototype.log = function(){
+Grid.prototype.log = function () {
     for (var i = 0; i < this.tiles.length; i++) {
         var row = this.tiles[i];
         console.log('| ' + row.join(' | ') + ' |');
@@ -156,25 +182,58 @@ Grid.prototype.log = function(){
     console.log('\r\n');
 };
 
-Grid.prototype.show = function() {
+Grid.prototype.show = function () {
 
-    var tbody  = document.createElement('tbody');
+    var grid = document.getElementById(this.id),
+        frag = document.createDocumentFragment();
 
     for (var i = 0; i < this.tiles.length; i++) {
-        var row = this.tiles[i],
-            tr = tbody.insertRow(i);
+        var row = this.tiles[i];
         for (var j = 0; j < row.length; j++) {
-            var td = tr.insertCell(j);
-            td.appendChild(document.createTextNode(row[j] !== 0 ? '' +  Math.pow(2, row[j]) : ''));
+
+            if(row[j]){
+                var left = j * this.tileSize + (j + 1) * this.separatorSize,
+                    top = i * this.tileSize + (i + 1) * this.separatorSize,
+                    tile;
+                if(row[j].id) {
+                    tile = document.getElementById(row[j].id);
+                    tile.replaceChild(document.createTextNode(
+                        row[j] ? '' + Math.pow(2, row[j].pow) : ''), tile.firstChild);
+
+                } else {
+
+                    tile = document.createElement('div');
+
+                    tile.id = row[j].id = '' + this.seq++;
+
+                    tile.style.width = this.tileSize + 'px';
+                    tile.style.height = this.tileSize + 'px';
+                    tile.style.lineHeight = this.tileSize + 'px';
+                    tile.appendChild(document.createTextNode(row[j] ? '' + Math.pow(2, row[j].pow) : ''));
+                    frag.appendChild(tile);
+                }
+
+                tile.className = 'tile';
+                tile.className += ' tile-' + row[j].pow;
+                tile.style.top = top + 'px';
+                tile.style.left = left + 'px';
+            }
         }
     }
 
-    var table = document.getElementById(this.table);
-    table.replaceChild(tbody, table.firstChild);
+    for (var k = 0; k < this.trash.length; k++) {
+        var del = document.getElementById(this.trash[k].id);
+        if(del) {
+            grid.removeChild(del);
+        }
+    }
+    this.trash = [];
+
+    grid.appendChild(frag);
 
 };
 
-(function(){
+(function () {
 
     var g = new Grid(4, 'grid');
 
@@ -182,7 +241,7 @@ Grid.prototype.show = function() {
     g.born();
     g.show();
 
-    window.addEventListener('keyup', function(e){
+    window.addEventListener('keyup', function (e) {
         switch (e.keyCode) {
             case 37:
                 g.slideLeft();
---
+++ js/Tile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Leo on 2015/5/8.
+ */
+
+function Tile(n,x,y){
+    this.pow = n;
+    this.x = x;
+    this.y = y;
+}
+
